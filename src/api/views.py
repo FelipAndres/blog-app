90e03b44ca464cbe76ebed384b105b706824c98d
FelipAndres <felipeandreso@outlook.com>
2020-12-27 14:13:54 -0300

    working api

--- src/api/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from rest_framework import generics
-from . serializers import EntradaSerializer
-from .models import EntradaModel, ExampleModel
-
-
-class EntradaView(generics.ListAPIView):
-    queryset = EntradaModel.objects.all()
-    serializer_class = EntradaSerializer
--- src/blog/api/models.py
+++ src/blog/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class EntradaModel(models.Model):
-    autor = models.CharField(max_length=50, unique=True)
+    autor = models.CharField(max_length=50)
     titulo = models.CharField(max_length=50)
     cuerpo = models.TextField(max_length=200)
 
